25886a193525cf7548957159774ceabe321309e7
Huan <hphan298@gmail.com>
2023-02-11 15:40:23 +0700

    Initial commit

---
+++ src/main/java/com/springboot/project4/project4backend/Project4BackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.project4.project4backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project4BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/project4/project4backend/Project4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.project4.project4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
