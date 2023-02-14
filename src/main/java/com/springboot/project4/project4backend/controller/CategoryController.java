03c21e5c8adb49be4c2fdb35ae560cac3f8341c5
huanphan <hphan298@gmail.com>
2023-02-14 17:53:56 +0700

    2-14-2023

--- src/main/java/com/springboot/project4/project4backend/controller/CategoryController.java
+++ src/main/java/com/springboot/project4/project4backend/controller/CategoryController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/categories")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class CategoryController {
     private CategoryService categoryService;
 
