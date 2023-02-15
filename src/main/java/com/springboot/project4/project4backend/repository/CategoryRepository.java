d89c3b8a56751837d513968f113ac8d4ff7ca0cc
Huan <hphan298@gmail.com>
2023-02-15 21:31:56 +0700

    2-15-2023

--- src/main/java/com/springboot/project4/project4backend/repository/CategoryRepository.java
+++ src/main/java/com/springboot/project4/project4backend/repository/CategoryRepository.java
@@ -2,9 +2,6 @@ package com.springboot.project4.project4backend.repository;
 
 import com.springboot.project4.project4backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
