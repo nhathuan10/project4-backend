2f62bb1f3a3af875258c15adca55b5faee6d3d46
Huan <hphan298@gmail.com>
2023-03-02 12:44:17 +0700

    3-2

--- src/main/java/com/springboot/project4/project4backend/controller/AuthController.java
+++ src/main/java/com/springboot/project4/project4backend/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class AuthController {
     private AuthService authService;
     private UserService userService;
